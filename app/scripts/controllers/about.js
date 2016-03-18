2cb74322241053530fc3e4f392beba78aaa3116b
Gonzalo Garcia <gonzalo@sedna.de>
2016-03-18 13:07:01 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ineNamesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ineNamesApp
+ */
+angular.module('ineNamesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ineNamesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ineNamesApp
+ */
+angular.module('ineNamesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
